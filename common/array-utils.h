6a2d439b0bbb5d952b8210b9d10cbb84c4a66746
Christopher Friedt <chris.f@pervices.com>
2017-07-11 18:05:12 +0000

    Add separate server process for flow control
    * Added 'flow' service
    * Removed flow-control port from 'server' service
    * Now 'server' only handles management port requests
    * Both 'flow' and 'server' services should be more responsive.
    * Tested on Cole

---
+++ common/array-utils.h
@@ -0,0 +1,8 @@
+#ifndef COMMON_ARRAY_UTILS_H_
+#define COMMON_ARRAY_UTILS_H_
+
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE( x ) ( sizeof( x ) / sizeof( (x)[ 0 ] ) )
+#endif
+
+#endif /* COMMON_ARRAY_UTILS_H_ */
