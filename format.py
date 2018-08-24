b2f12ec01de5b287b52e120d6a96129201264204
Gustav Louw <airglouw@gmail.com>
2018-08-24 19:27:20 +0000

    formatter now excludes build folder (which is third party)

--- format.py
+++ format.py
@@ -4,4 +4,7 @@ for subdir, dirs, files in os.walk("."):
     for file in files:
         if file.endswith(('.c', '.h')):
             path = os.path.join(subdir, file)
-            os.system("clang-format -style='{SortIncludes: false, IndentWidth: 4, ColumnLimit: 80, AllowShortBlocksOnASingleLine: false}' %s" % path)
+            if "build" in path:
+                continue
+            else:
+                os.system("clang-format -style='{SortIncludes: false, IndentWidth: 4, ColumnLimit: 80, AllowShortBlocksOnASingleLine: false}' %s" % path)
