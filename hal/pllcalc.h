07c3059fe254b75cff1a0ce25a5fd1adb4d81eaa
Victor Wollesen <victor.w@pervices.com>
2020-01-20 18:52:03 +0000

    PLLCALC: Fix commenting line breaks.

--- hal/pllcalc.h
+++ hal/pllcalc.h
@@ -44,8 +44,9 @@
 #include <stdlib.h>
 
 // For stand alone operation;
-//#define _PLL_DEBUG_STANDALONE //Standalone program, compile with: gcc -lm
-// pllcalc.c #define _PLL_DEBUG_INFO #define _PLL_DEBUG_VERBOSE
+//#define _PLL_DEBUG_STANDALONE //Standalone program, compile with: gcc -lm pllcalc.c
+//#define _PLL_DEBUG_INFO
+//#define _PLL_DEBUG_VERBOSE
 
 #ifndef _PLL_DEBUG_STANDALONE
 #include "common.h"
