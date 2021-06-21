082bdaef169698ff45af0d849b6c5b17c3825357
John Polak <john.p@pervices.com>
2021-06-21 18:59:20 +0000

    TATE_8R: base sample rate is now 1GSPS

--- hal/properties_tate_8r.c
+++ hal/properties_tate_8r.c
@@ -39,7 +39,7 @@
 #include "gpio_pins.h"
 
 // Sample rates are in samples per second (SPS).
-#define BASE_SAMPLE_RATE   250000000.0  //After base rate
+#define BASE_SAMPLE_RATE   1000000000.0  //After base rate
 #define RESAMP_SAMPLE_RATE 160000000.0  //After 4/5 resampling //NB: Tate 64t does NOT support 4/5 resampling
 // (2 ^ 32) / (BASE_SAMPLE_RATE)
 #define DSP_NCO_CONST \
