5b7fcb2b0365b89d7f70f310761b34403f633b2c
Doug W <doug.w@pervices.com>
2021-07-21 20:55:22 +0000

    Disabled setting values for off in memory

--- hal/properties_tate_4r4t.c
+++ hal/properties_tate_4r4t.c
@@ -2218,8 +2218,8 @@ CHANNELS
             sprintf(pwr_cmd, "rfe_control %d off", INT_RX(ch));                   \
             /*system(pwr_cmd);*/                                                   \
                                                                                \
-            rx_power[INT_RX(ch)] = PWR_OFF;                                       \
-            rx_stream[INT_RX(ch)] = STREAM_OFF;                                   \
+            /*rx_power[INT_RX(ch)] = PWR_OFF;*/                                       \
+            /*rx_stream[INT_RX(ch)] = STREAM_OFF;*/                                   \
                                                                                \
             /* kill the channel */                                             \
             strcpy(buf, "board -c " STR(ch) " -k\r");                          \
