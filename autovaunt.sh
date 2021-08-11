3319eb2333ec96b331c25991c2f58796d84086f2
Doug W <doug.w@pervices.com>
2021-08-11 17:26:47 +0000

    VAUNT: added  script that cleanly builds vaunt

---
+++ autovaunt.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+./autogen.sh clean
+./autogen.sh
+
+./configure                                             \
+        --prefix=/usr                                       \
+        --host=x86_64                                       \
+        CC="arm-unknown-linux-gnueabihf-gcc"                \
+        CFLAGS="-Wall -O3 -pipe -fomit-frame-pointer -Wall  \
+            -march=armv7-a -mtune=cortex-a9 -mfpu=neon"     \
+        CPPFLAGS="-Wall -O3 -pipe -fomit-frame-pointer -Wall\
+            -march=armv7-a -mtune=cortex-a9 -mfpu=neon"     \
+        CXX="arm-unknown-linux-gnueabihf-g++"               \
+        CXXFLAGS="-Wall -O3 -pipe -fomit-frame-pointer -Wall\
+            -march=armv7-a -mtune=cortex-a9 -mfpu=neon"     \
+        PRODUCT=VAUNT
+
+make -j9
