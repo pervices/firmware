589478ae6a7354ec6cba44215f7ffeeace846114
Doug W <doug.w@pervices.com>
2021-09-15 16:15:52 +0000

    Added function to write to jesd registers

--- hal/drivers/mmap/mmap.c
+++ hal/drivers/mmap/mmap.c
@@ -196,9 +196,9 @@ void mmap_fini() {
 }
 
 #if defined TATE_4R4T || defined TATE_8R
-//the jesd_num in this is the one used for the bitshift to select the which jesd's registers to access
-uint32_t read_jesd_reg(uint8_t jesd_num, uint32_t address, uint32_t *data) {
-    uint32_t jesd_id = 1 << jesd_num;
+//the jesd_shift in this is the used for the bitshift to select the which jesd's registers to access
+uint32_t read_jesd_reg(uint8_t jesd_shift, uint32_t address, uint32_t *data) {
+    uint32_t jesd_id = 1 << jesd_shift;
     int write_code = write_hps_reg("net6", jesd_id);
     write_hps_reg("net7", address/4);
     write_hps_reg("net9", 0x1);
@@ -206,5 +206,15 @@ uint32_t read_jesd_reg(uint8_t jesd_num, uint32_t address, uint32_t *data) {
     uint32_t tmp;
     read_hps_reg("res_ro30", &tmp);
     *data = tmp;
+}
+
+//the jesd_shift in this is the one used for the bitshift to select the which jesd's registers to access
+uint32_t write_jesd_reg(uint8_t jesd_shift, uint32_t address, uint32_t data) {
+    uint32_t jesd_id = 1 << jesd_shift;
+    int write_code = write_hps_reg("net6", jesd_id);
+    write_hps_reg("net7", address/4);
+    write_hps_reg("net8", data);
+    write_hps_reg("net9", 0x2);
+    write_hps_reg("net9", 0x0);
 }
 #endif
