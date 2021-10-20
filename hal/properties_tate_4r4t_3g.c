097477ad5e51dcfb7e11b42049e1958381dfecb6
Doug W <doug.w@pervices.com>
2021-10-20 20:19:44 +0000

    TATE_4R4T_3G: stop setting sysref mask when reseting jesd

--- hal/properties_tate_4r4t_3g.c
+++ hal/properties_tate_4r4t_3g.c
@@ -2543,9 +2543,10 @@ CHANNELS
         \
         /*enables responding to sysref*/\
         strcpy(buf, "board -s 1\r");\
-        ping_rx(uart_rx_fd[INT_RX(ch)], (uint8_t *)buf, strlen(buf), INT(ch));\
+        /*ping_rx(uart_rx_fd[INT_RX(ch)], (uint8_t *)buf, strlen(buf), INT(ch));*/\
         \
         uint32_t individual_reset_bit = 1 << (INT(ch) + INDIVIDUAL_RESET_BIT_OFFSET_RX);\
+        PRINT(INFO, "individual_reset_bit: %i", individual_reset_bit);\
         write_hps_reg("res_rw7",  individual_reset_bit);\
         /*this wait is needed*/\
         usleep(300000);\
@@ -2558,7 +2559,7 @@ CHANNELS
         \
         /*disbale responding to sysref*/\
         strcpy(buf, "board -s 0\r");\
-        ping_rx(uart_rx_fd[INT_RX(ch)], (uint8_t *)buf, strlen(buf), INT(ch));\
+        /*ping_rx(uart_rx_fd[INT_RX(ch)], (uint8_t *)buf, strlen(buf), INT(ch));*/\
         return RETURN_SUCCESS;                                                 \
     }                                                                          \
     \
