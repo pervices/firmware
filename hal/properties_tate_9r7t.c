2f565e8578de35535ec9c4c70ff81f177c91d6b4
Doug W <doug.w@pervices.com>
2022-08-09 21:17:20 +0000

    TATE_9R7T: removing ip swap for upper tx channels
    This was intended as a fix to an issue that looked like an iq swap but
    turned out to be the nco polarity not working. It has been fixed on the
    FPGA

--- hal/properties_tate_9r7t.c
+++ hal/properties_tate_9r7t.c
@@ -1420,18 +1420,10 @@ static void ping_tx(const int fd, uint8_t *buf, const size_t len, int ch) {
         sscanf(data, "%i", &swap);                                           \
         uint32_t old_val = 0;                                                      \
         read_hps_reg(tx_reg4_map[INT(ch)], &old_val);                          \
-        /* There is an issue that results in an iq swap on the last 3 channels, temporary fix until a root cause can be found */\
-        if(INT(ch) != 4 && INT(ch) != 5 && INT(ch) !=6) {\
-            if ( swap == 1)                                            \
-                write_hps_reg(tx_reg4_map[INT(ch)], old_val | (1 << 12));          \
-            else                                                                   \
-                write_hps_reg(tx_reg4_map[INT(ch)], old_val & ~(1 << 12));         \
-        } else {\
-            if ( swap != 1)                                            \
-                write_hps_reg(tx_reg4_map[INT(ch)], old_val | (1 << 12));          \
-            else                                                                   \
-                write_hps_reg(tx_reg4_map[INT(ch)], old_val & ~(1 << 12));         \
-        }\
+        if ( swap == 1)                                            \
+            write_hps_reg(tx_reg4_map[INT(ch)], old_val | (1 << 12));          \
+        else                                                                   \
+            write_hps_reg(tx_reg4_map[INT(ch)], old_val & ~(1 << 12));         \
                                                                                \
         return RETURN_SUCCESS;                                                 \
     }                                                                          \
