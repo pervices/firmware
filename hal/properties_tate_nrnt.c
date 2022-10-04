8423e2e8f804d4ffb02524e5586c1f14cd1d270e
Doug W <doug.w@pervices.com>
2022-10-04 18:41:39 +0000

    TATE_NRNT: flipping bit order for which SFP present

--- hal/properties_tate_nrnt.c
+++ hal/properties_tate_nrnt.c
@@ -3807,6 +3807,8 @@ static int hdlr_fpga_link_sfp_reset(const char *data, char *ret) {
         // The first 4 bits indicate which sfp ports have cables attatched, the next 4 indicate which links are established
         uint32_t sfp_link_established = (sys18_val >> 24 & 0xf);
         uint32_t sfp_module_present = sys18_val >> 28;
+        // The bits used to detect which SFPs are inserted is flipped compared to which SFPs are working
+        sfp_module_present = ((sfp_module_present & 0x1) << 3) | ((sfp_module_present & 0x2) << 1) | ((sfp_module_present & 0x4) >> 1) | ((sfp_module_present & 0x8) >> 3);
         uint32_t sfp_errors = sys18_val & 0xe000;
 
         if(!sfp_errors && (sfp_link_established == sfp_module_present)) {
