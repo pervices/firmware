d5becdd5d3c68637d66f9b8823f114b57f28d811
Doug W <doug.w@pervices.com>
2023-08-02 20:29:13 +0000

    Updating autovaunt to master

--- autovaunt.sh
+++ autovaunt.sh
@@ -1,19 +1,34 @@
 #!/bin/sh
 
+if [ -z $CC ]; then
+	SERVER_CC="arm-unknown-linux-gnueabihf-gcc"
+else 
+	SERVER_CC=$CC
+fi
+
+if [ -z $CXX ]; then
+	SERVER_CXX="arm-unknown-linux-gnueabihf-g++"
+else
+	SERVER_CXX=$CXX
+fi
+
 ./autogen.sh clean
 ./autogen.sh
+
 
 ./configure                                             \
         --prefix=/usr                                       \
         --host=x86_64                                       \
-        CC="arm-unknown-linux-gnueabihf-gcc"                \
+	CC=$SERVER_CC \
         CFLAGS="-Wall -O3 -pipe -fomit-frame-pointer -Wall  \
-            -march=armv7-a -mtune=cortex-a9 -mfpu=neon"     \
+            -march=armv7-a -mtune=cortex-a9 -mfpu=neon     \
+            -Werror -lm" \
         CPPFLAGS="-Wall -O3 -pipe -fomit-frame-pointer -Wall\
             -march=armv7-a -mtune=cortex-a9 -mfpu=neon"     \
-        CXX="arm-unknown-linux-gnueabihf-g++"               \
+	CXX=$SERVER_CXX \
         CXXFLAGS="-Wall -O3 -pipe -fomit-frame-pointer -Wall\
             -march=armv7-a -mtune=cortex-a9 -mfpu=neon"     \
-        PRODUCT=VAUNT
+        PRODUCT=VAUNT\
+        HW_REV=$1
 
-make -j9
+make -j$(nproc)
