eec9d8c54407fc6c3f3191fa3db552580967fba9
Charles Smith <charles.s@pervices.com>
2023-09-11 19:01:57 +0000

    Allow lowercase input for autovaunt

--- autovaunt.sh
+++ autovaunt.sh
@@ -29,7 +29,7 @@ fi
         CXXFLAGS="-Wall -O3 -pipe -fomit-frame-pointer -Wall\
             -march=armv7-a -mtune=cortex-a9 -mfpu=neon"     \
         PRODUCT="VAUNT" \
-        HW_REV="$1" \
+        HW_REV="${1^^}" \
         NRX="R4" \
         NTX="T4" \
         MAX_RATE="S162M5"
