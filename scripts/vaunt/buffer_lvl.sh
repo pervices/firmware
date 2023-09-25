c698a208198c6a0d481d73ab9a58c266030e5e78
Victor Wollesen <victor.w@pervices.com>
2023-09-25 15:15:26 +0000

    SCRIPTS,VAUNT: Adding channel selection logic to vaunt buffer_lvl.sh

--- scripts/vaunt/buffer_lvl.sh
+++ scripts/vaunt/buffer_lvl.sh
@@ -1,14 +1,51 @@
 #!/bin/bash
 clear
-#max =0;
-#maxperc =0;
+set  -euo pipefail
+
+#echo Script Name: $0
+#echo Pass Channel to monitor as a,b,c or d.
+#echo This version can only monitor one channel at a time.
+
+bufferlevel=0
+channel=a
+
+if [ $# -eq 1 ]
+then
+  channel=${1^^}
+fi
+
+#Crimson specific values
+high=2*32767;
+max=0;
+maxperc=0;
+
+readbuffer() {
+	channel=$1
+	if [[ "$channel" == "A" ]]
+	then
+		eval bufferlevel=$(mem rr res_ro4);
+	elif [[ "$channel" == "B" ]]
+	then
+		eval bufferlevel=$(mem rr res_ro5)
+	elif [[ "$channel" == "C" ]]
+	then
+		eval bufferlevel=$(mem rr res_ro6)
+	elif [[ "$channel" == "D" ]]
+	then
+		eval bufferlevel=$(mem rr res_ro7)
+	else 
+		eval bufferlevel=$(mem rr res_ro4)
+	fi
+	
+}
+
 while true; do
-	buff_lvl=$(mem rr res_ro4);
+	readbuffer $channel;
+	buff_lvl=$bufferlevel;
 	buff_lvl=$(($buff_lvl));
-	high=2*32767;
 	
 	if [[ "$buff_lvl" > "$max" ]]
-	then
+       	then
 		max=$buff_lvl;
 	fi
 	buff_percent=$((100*buff_lvl));
