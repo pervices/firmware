224e74851a858bbec8c6da315a9705353a5b217c
Doug W <doug.w@pervices.com>
2023-11-06 17:29:58 +0000

    MCU: repairing on Vaunt after Vaunt-Tate merge broke it
    Also refactored to avoid hard coded file descriptor

--- hal/comm_manager.c
+++ hal/comm_manager.c
@@ -100,9 +100,10 @@ static int get_next_uart_fd(int *fd) {
 }
 
 int init_uart_comm(int *fd, const char *dev, uint16_t options) {
-
-    if (get_next_uart_fd(fd) < 0)
+    if (get_next_uart_fd(fd) < 0) {
+        PRINT(ERROR, "Unable to get file descriptor for %s\n", dev);
         return RETURN_ERROR_INSUFFICIENT_RESOURCES;
+    }
 
     PRINT(VERBOSE, "Found file descriptor %i\n", *fd);
     used_uart_devices[*fd] = USED_DEVICE;
@@ -148,8 +149,10 @@ int recv_uart_comm(int fd, uint8_t *data, uint32_t *size, int32_t max_size) {
 }
 
 int send_uart_comm(int fd, uint8_t *data, uint16_t size) {
-    if (fd < 0)
+    if (fd < 0) {
+        PRINT(ERROR, "File descriptor not initialized: %i\n", fd);
         return RETURN_ERROR_PARAM;
+    }
     int mydev = uart_devices[fd];
     return send_uart(mydev, data, size);
 }
