37e8dc6928e9b47704133a47d827a41f7533739d
Doug W <doug.w@pervices.com>
2024-02-29 23:08:32 +0000

    Disable reinit on error

--- hal/properties_tate_nrnt.c
+++ hal/properties_tate_nrnt.c
@@ -5118,7 +5118,11 @@ static int hdlr_fpga_reset(const char *data, char *ret) {
     set_led_state(led_state);
 
     // Prevent FPGA JESD from being triggered by continuous sysref when powering on boards or? prevents it from tiggering rx to start JESD early - maybe hopefully
+
     for(uint8_t n = 0; n < NUM_RX_CHANNELS; n++) {
+        // Turns on reinit on error (which will be occuring continuously until its time to reinit JESD)
+        write_jesd_reg(1 << n, 0x50, 0x1000);
+        // Disables csr_dis_lane_align_det
         write_jesd_reg(1 << n, 0x54, 0x0);
     }
 
