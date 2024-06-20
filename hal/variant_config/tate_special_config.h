948ef5700069121a3154aba29877ef9389e12e22
Doug W <doug.w@pervices.com>
2024-06-20 20:43:49 +0000

    TATE: adding flags for LOIN

--- hal/variant_config/tate_special_config.h
+++ hal/variant_config/tate_special_config.h
@@ -22,16 +22,31 @@
 //Decodes flags for special variant
 // Currently implemented mask values:
 // bit 0: sets flag USE_3G_AS_1G. Uses 1GSPS sample rate with 3GSPS backplane, can use 3GSPS or 1GSPS RFEs. The server will set 3GSPS RX boards programmed with Rx3 code to 1GSPS mode. TX boards must be programmed with regular (1GSPS) Tx mcu. Time board must be programmed with Time1on3 mcu if 3GSPS RX boards, or regular (1GSPS) time mcu if 1GSPS RX boards.
+// bit 1: user_lo
 
 // Decode bitmask
-#ifdef F1
+#ifdef F3
     #define USE_3G_AS_1G 1
     #define S_USE_3G_AS_1G "1"
+    #define USER_LO 1
+
+#elif defined(F2)
+    #define USE_3G_AS_1G 0
+    #define S_USE_3G_AS_1G "0"
+    #define USER_LO 1
+
+#elif defined(F1)
+    #define USE_3G_AS_1G 1
+    #define S_USE_3G_AS_1G "1"
+    #define USER_LO 0
+
 #elif defined(F0)
     #define USE_3G_AS_1G 0
     #define S_USE_3G_AS_1G "0"
+    #define USER_LO 0
+
 #else
-    #error "This file must be compiled with a valid flag combination (F0, F1)"
+    #error "This file must be compiled with a valid flag combination (F0, F1, F2, F3)"
 #endif
 
 // NCO shift in the ADC in 1Gsps mode of the ADC32RF45
