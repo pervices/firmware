e22d245eca4f013464a7667cb63fd20b58bfbc50
Doug W <doug.w@pervices.com>
2024-08-21 16:57:30 +0000

    TATE: initial support for RTM6
    Also enables Lily RTM1
    
    It is currently the same as RTM5. Theoretically the only change required will
    be optimizing lo power

--- hal/variant_config/tate_rtm_config.h
+++ hal/variant_config/tate_rtm_config.h
@@ -30,8 +30,11 @@
     #elif defined(RTM5)
         #define PRODUCT_RTM_VER 5
         #define HARDWARE_RTM_VER 5
+    #elif defined(RTM6)
+        #define PRODUCT_RTM_VER 6
+        #define HARDWARE_RTM_VER 6
     #else
-        #error "TATE_NRNT must be compiled with a valid hardware revision (RTM3, RTM4, RTM5)"
+        #error "TATE_NRNT must be compiled with a valid hardware revision (RTM3, RTM4, RTM6)"
     #endif
 #elif defined(LILY)
     // No actual Lily version exists based on RTM5 hardware, this option exists for testing using a converted RTM5 Tate
@@ -48,7 +51,7 @@
     #error "This file must be compiled with a product revision (TATE_NRNT, LILY)"
 #endif
 
-#if HARDWARE_RTM_VER == 3
+#if HARDWARE_RTM_VER == 3 || HARDWARE_RTM_VER == 4
     // On 1Gsps sysref is activated on the falling edge of sysref, other stuff is active on the rising edge
     // To compensate for this some revisions swapped dev clk on hardware
     #define INVERT_DEV_CLOCK_HARDWARE_SWAP 1
@@ -58,17 +61,7 @@
     // Otherwise delay = n * 300ps + 300ps
     #define DEFAULT_ANALOG_SYSREF_DELAY 0
 
-#elif HARDWARE_RTM_VER == 4
-    // On 1Gsps sysref is activated on the falling edge of sysref, other stuff is active on the rising edge
-    // To compensate for this some revisions swapped dev clk on hardware
-    #define INVERT_DEV_CLOCK_HARDWARE_SWAP 1
-
-    // Default analog delay
-    // 0 = 0
-    // Otherwise delay = n * 300ps + 300ps
-    #define DEFAULT_ANALOG_SYSREF_DELAY 0
-
-#elif HARDWARE_RTM_VER == 5
+#elif HARDWARE_RTM_VER == 5 || HARDWARE_RTM_VER == 6
     // On 1Gsps sysref is activated on the falling edge of sysref, other stuff is active on the rising edge
     // To compensate for this some revisions swapped dev clk on hardware
     #define INVERT_DEV_CLOCK_HARDWARE_SWAP 0
@@ -79,5 +72,17 @@
     #define DEFAULT_ANALOG_SYSREF_DELAY 0
 
 #else
-    #error "Invalid HARDWARE_RTM_VER (derived from product and hardware revision). Valid HARDWARE_RTM_VER: 3, 4, 5"
+    #error "Invalid HARDWARE_RTM_VER (derived from product and hardware revision). Valid HARDWARE_RTM_VER: 3, 4, 5, 6"
+#endif
+
+#if HARDWARE_RTM_VER == 3 || HARDWARE_RTM_VER == 4 || HARDWARE_RTM_VER == 5
+    #define TX_LO_POWER 60
+    #define RX_LO_POWER 45
+
+#elif HARDWARE_RTM_VER == 6
+    // TODO: optimize lo power for RTM6
+    #define TX_LO_POWER 60
+    #define RX_LO_POWER 45
+#else
+    #error "Invalid HARDWARE_RTM_VER (derived from product and hardware revision). Valid HARDWARE_RTM_VER: 3, 4, 5, 6"
 #endif
