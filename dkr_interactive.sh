44aa0309af0d56ea544524f14ffb50a5493fd7e6
John Polak <john.p@pervices.com>
2025-03-18 15:42:06 +0000

    DKR-INTERACTIVE: add present working directory as safe in git
    This allows docker to read the current git commit and save that information into the compiled files.

--- dkr_interactive.sh
+++ dkr_interactive.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -it --volume $(pwd):$(pwd) --workdir $(pwd) pvtesting/archlinux:20230619.0.159280 /bin/bash
+DOCKER_ID=$(docker run -dit --volume $(pwd):$(pwd) --workdir $(pwd) pvtesting/archlinux:20230619.0.159280 /bin/bash)
+docker exec -d $DOCKER_ID git config --global --add safe.directory $(pwd)
+docker attach $DOCKER_ID
