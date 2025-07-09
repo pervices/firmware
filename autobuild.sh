de88b8d70a9f8b9b1888a8d0ee024a81eb95368f
Joseph Talon <joseph.t@pervices.com>
2025-07-09 20:57:21 +0000

    SCRIPT: Added compile commands to autobuild. Updated autotate/autovaunt to call autobuild

--- autobuild.sh
+++ autobuild.sh
@@ -1,24 +1,43 @@
 #!/bin/sh
 
+# Used for validating values from user
+VALID_PRODUCTS=('TATE_NRNT' 'LILY' 'VAUNT')
+VAUNT_RTMS=('RTM6' 'RTM7' 'RTM8' 'RTM9' 'RTM10' 'RTM11' 'RTM12')
+VAUNT_RATES=('162M5')
+TATE_RTMS=('RTM3' 'RTM4' 'RTM5' 'RTM6' 'RTM7')
+TATE_RATES=('1000' '3000')
+LILY_RTMS=('RTM0' 'RTM1')
+LILY_RATES=('500')
+
+
 function print_help() {
-    echo ""
     echo "Compiles server for device with provided configuration options."
     echo "Usage:"
-    echo "  ./autobuild.sh []"
+    echo "  ./autobuild.sh -p <TATE_NRNT|LILY|VAUNT> -v <RTMx> -r <num_rx> -t <num_tx> -s <max_sample_rate> [optional flags]..."
+    echo "  ./autobuild.sh [-h|--help]  # Prints this help menu"
     echo ""
     echo "Required:"
     echo "  -p, --product       The product to compile the server for (TATE_NRNT|LILY|VAUNT)"
     echo "  -v, --hw-revision   The hardware revision to compile for (RTM5, RTM10, etc...)"
-    echo "  -r, --rx-channels   The number of RX channels on the device"
-    echo "  -t, --tx-channels   The number of TX channels on the device"
-    echo "  -s, --max-rate      The max sample rate of the device (3000|1000|500|162.5)"
+    echo "  -r, --rx-channels   The number of Rx channels on the device"
+    echo "  -t, --tx-channels   The number of Tx channels on the device"
+    echo "  -s, --max-rate      The max sample rate of the device (3000|1000|500|162M5)"
     echo ""
     echo "Optional:"
-    echo "  --rx_40ghz_fe       Indicates the device tx board has been replaced by a 40GHz RX frontend"
+    echo "  --rx_40ghz_fe       Indicates the device Tx board has been replaced by a 40GHz Rx frontend"
     echo "  --use_3g_as_1g      Use 1GSPS sample rate with 3GSPS backplane"
-    echo "  --user_lo       "
+    echo "  --user_lo           Enable user LO mode"
+    echo ""
+    echo "Examples:"
+    echo "  ./autobuild.sh -p VAUNT -v RTM10 -r 4 -t 4 -s 162M5                     # Vaunt RTM10"
+    echo "  ./autobuild.sh -p TATE_NRNT -v RTM5 -r 4 -t 4 -s 1000 --use_3g_as_1g    # Tate RTM5 using 3G backplane as 1G"
+    echo "  ./autobuild.sh -p LILY -v RTM1 -r 4 -t 4 -s 500                         # Lily RTM1"
 }
 
+# Validate the value provided for an argument
+function validate_value() {
+
+}
 
 # Required arguments
 PRODUCT=
@@ -41,7 +60,7 @@ while [ $# -gt 0 ]; do
         -p|--product)
             if [ -z $2 ]; then
                 echo "[ERROR] Could not find value for -p|--product flag."
-                echo "  Make sure you provide a valid product after the product flag."
+                echo "        Make sure you provide a valid product after the product flag."
                 exit 1
             fi
             PRODUCT="${2^^}"
@@ -51,7 +70,8 @@ while [ $# -gt 0 ]; do
         -v|--hw-revision)
             if [ -z $2 ]; then
                 echo "[ERROR] Could not find value for -v|--hw-revision flag."
-                echo "  Make sure you provide a hardware revision after the hardware revision flag."
+                echo "        Make sure you provide a valid RTM number after the hardware revision flag."
+                exit 1
             fi
             HW_REV="${2^^}"
             shift
@@ -60,7 +80,8 @@ while [ $# -gt 0 ]; do
         -r|--rx-channels)
             if [ -z $2 ]; then
                 echo "[ERROR] Could not find value for -r|--rx-channels flag."
-                echo "  Make sure you provide a value after the rx-channels flag."
+                echo "        Make sure you provide a value after the rx-channels flag."
+                exit 1
             fi
             NUM_RX="R$2"
             shift
@@ -69,7 +90,8 @@ while [ $# -gt 0 ]; do
         -t|--tx-channels)
             if [ -z $2 ]; then
                 echo "[ERROR] Could not find value for -t|--tx-channels flag."
-                echo "  Make sure you provide a value after the rx-channels flag."
+                echo "        Make sure you provide a value after the rx-channels flag."
+                exit 1
             fi
             NUM_TX="T$2"
             shift
@@ -78,7 +100,8 @@ while [ $# -gt 0 ]; do
         -s|--max-rate)
             if [ -z $2 ]; then
                 echo "[ERROR] Could not find value for -s|--max-rate flag."
-                echo "  Make sure you provide a value after the max rate flag."
+                echo "        Make sure you provide a value after the max rate flag."
+                exit 1
             fi
             MAX_RATE=$2
             shift
@@ -106,8 +129,6 @@ while [ $# -gt 0 ]; do
     let n=$n+1
 done
 
-VALID_PRODUCTS=('TATE_NRNT' 'LILY' 'VAUNT')
-
 # Check for all required arguments
 if [ -z $PRODUCT ]; then
     echo "[ERROR] Product was not specified but is required for compilation."
@@ -133,6 +154,31 @@ if [ -z $HW_REV ]; then
     exit 1
 fi
 
+if [ $PRODUCT == "VAUNT" ]; then
+    VALID_RTMS=${VAUNT_RTMS[@]}
+    VALID_RATES=${VAUNT_RATES[@]}
+elif [ $PRODUCT == "TATE_NRNT" ]; then
+    VALID_RTMS=${TATE_RTMS[@]}
+    VALID_RATES=${TATE_RATES[@]}
+elif [ $PRODUCT == "LILY" ]; then
+    VALID_RTMS=${LILY_RTMS[@]}
+    VALID_RATES=${LILY_RATES[@]}
+fi
+
+is_valid=0
+for rtm in ${VALID_RTMS[@]}; do
+    if [ $HW_REV == $rtm ]; then
+        is_valid=1
+        break
+    fi
+done
+
+if [ ${!is_valid} ]; then
+    echo "[ERROR] Invalid RTM: $HW_REV"
+    echo "  Valid RTMS for specified product are: ${VALID_RTMS[@]}"
+    exit 1
+fi
+
 if [ -z $NUM_RX ]; then
     echo "[ERROR] Number of RX channels was not specified but is required for compilation."
     exit 1
@@ -148,4 +194,79 @@ if [ -z $MAX_RATE ]; then
     exit 1
 fi
 
+# TODO: Move validation to function
+is_valid=0
+for rate in ${VALID_RATES[@]}; do
+    if [ $MAX_RATE == $rate ]; then
+        is_valid=1
+        break
+    fi
+done
+
+if [ ${!is_valid} ]; then
+    echo "[ERROR] Invalid max sample rate: $MAX_RATE"
+    echo "  Valid rates for specified product are: ${VALID_RATES[@]}"
+    exit 1
+fi
+
+if [ "${PRODUCT}" == "VAUNT" ]; then
+    if [ -z $CC ]; then
+    if command -v arm-linux-gnueabihf-gcc 2>&1 >/dev/null; then
+        SERVER_CC="arm-linux-gnueabihf-gcc"
+    elif command -v arm-unknown-linux-gnueabihf-gcc 2>&1 >/dev/null; then
+        SERVER_CC="arm-unknown-linux-gnueabihf-gcc"
+        echo "WARNING: using different compiler than CI system" # see pvpkg/firmware-scripts/server.sh
+    else
+        echo "ERROR: GCC compiler for ARM not found"
+        exit 1
+    fi
+    else
+        SERVER_CC=$CC
+    fi
+
+    if [ -z $CXX ]; then
+        if command -v arm-linux-gnueabihf-g++ 2>&1 >/dev/null; then
+            SERVER_CXX="arm-linux-gnueabihf-g++"
+        elif command -v arm-unknown-linux-gnueabihf-g++ 2>&1 >/dev/null; then
+            SERVER_CXX="arm-unknown-linux-gnueabihf-g++"
+            echo "WARNING: using different compiler than CI system" # see pvpkg/firmware-scripts/server.sh
+        else
+            echo "ERROR: G++ compiler for ARM not found"
+            exit 1
+        fi
+    else
+        SERVER_CXX=$CXX
+    fi
+
+    PRODUCT_CFLAGS="-Wall -O3 -pipe -fomit-frame-pointer -Wfatal-errors  \
+                    -march=armv7-a -mtune=cortex-a9 -mfpu=neon"
+elif [ "${PRODUCT}" == "TATE_NRNT" ] || [ "${PRODUCT}" == "LILY" ]; then
+    SERVER_CC="aarch64-linux-gnu-gcc"
+    SERVER_CXX="aarch64-linux-gnu-g++"
+    PRODUCT_CFLAGS="-Wall -O3 -pipe -fomit-frame-pointer -Wfatal-errors \
+                    -march=armv8-a -mtune=cortex-a53"
+fi
+
+./autogen.sh clean
+./autogen.sh
+
+./configure                         \
+    --prefix=/usr                   \
+    --host=x86_64                   \
+    CC=$SERVER_CC                   \
+    CFLAGS="${PRODUCT_CFLAGS}       \
+            -Werror -lm -pthread"   \
+    CPPFLAGS="$PRODUCT_CFLAGS"      \
+    CXX=$SERVER_CXX                 \
+    CXXFLAGS="$PRODUCT_CFLAGS"      \
+    PRODUCT=$PRODUCT                \
+    HW_REV=$HW_REV                  \
+    NRX=$NUM_RX                     \
+    NTX=$NUM_TX                     \
+    MAX_RATE="S${MAX_RATE}"         \
+    RX_40GHZ_FE=$RX_40GHZ_FE        \
+    USE_3G_AS_1G=$USE_3G_AS_1G      \
+    USER_LO=$USER_LO                \
+
+make -j$(nproc)
 
