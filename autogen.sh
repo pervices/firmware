6376f90de143b9b3a96e473787c956d2f6f05384
John Polak <john.p@pervices.com>
2025-09-04 16:04:23 +0000

    AUTOGEN.SH: refactor to avoid too many arguments to rm and catch failures to remove files

--- autogen.sh
+++ autogen.sh
@@ -4,47 +4,50 @@ if [ "$1" = "clean" ]; then
   make clean &>/dev/null
   make distclean &>/dev/null
   make maintainer-clean &>/dev/null
-  rm -Rf \
-    $(find . \
-      -name '*~' \
-      -o -name 'config.h' \
-      -o -name 'config.h.in' \
-      -o -name 'Makefile.in' \
-      -o -name 'Makefile' \
-      -o -name 'aclocal.m4' \
-      -o -name 'ar-lib' \
-      -o -name 'autom4te.cache' \
-      -o -name 'compile' \
-      -o -name 'py-compile' \
-      -o -name 'config.guess' \
-      -o -name 'config.status' \
-      -o -name 'config.sub' \
-      -o -name 'configure' \
-      -o -name 'depcomp' \
-      -o -name 'install-sh' \
-      -o -name 'ltmain.sh' \
-      -o -name 'libtool' \
-      -o -name 'libtool.m4' \
-      -o -name 'ltoptions.m4' \
-      -o -name 'ltsugar.m4' \
-      -o -name 'ltversion.m4' \
-      -o -name 'lt~obsolete.m4' \
-      -o -name 'm4' \
-      -o -name 'missing' \
-      -o -name 'stamp-h1' \
-      -o -name 'test-driver' \
-      -o -name '.deps' \
-      -o -name '.libs' \
-      -o -name '.dirstamp' \
-      -o -name '*.o' \
-      -o -name '*.lo' \
-      -o -name '*.la' \
-      -o -name '*.log' \
-      -o -name '*.scan' \
-      -o -name 'INSTALL' \
-      -o -name '*.doxyfile' \
+  rc=0
+  clean_list=('*~' \
+      'config.h' \
+      'config.h.in' \
+      'Makefile.in' \
+      'Makefile' \
+      'aclocal.m4' \
+      'ar-lib' \
+      'autom4te.cache' \
+      'compile' \
+      'py-compile' \
+      'config.guess' \
+      'config.status' \
+      'config.sub' \
+      'configure' \
+      'depcomp' \
+      'install-sh' \
+      'ltmain.sh' \
+      'libtool' \
+      'libtool.m4' \
+      'ltoptions.m4' \
+      'ltsugar.m4' \
+      'ltversion.m4' \
+      'lt~obsolete.m4' \
+      'm4' \
+      'missing' \
+      'stamp-h1' \
+      'test-driver' \
+      '.deps' \
+      '.libs' \
+      '.dirstamp' \
+      '*.o' \
+      '*.lo' \
+      '*.la' \
+      '*.log' \
+      '*.scan' \
+      'INSTALL' \
+      '*.doxyfile' \
   )
-  exit 0
+  for i in ${clean_list[@]};
+  do
+    rm -rf $(find -name $i) || rc=$?
+  done
+  exit $rc
 fi
 
 autoreconf -vif
