8bbfeacb02c77f92784de736ec8e5548a92a5a83
Doug W. <doug.w@pervices.com>
2026-05-19 14:22:13 +0000

    PROPERTY_MANAGER: replacing system with libc in symlink creation
    Replacing creating symlink using system() with libc function and adding
    error checking.
    
    I also removed the part where it deletes any existing file where the
    symlink is meant to be created. Said function was created because we
    used to not clear the state tree between boots, and didn't have error
    checking. This meant that if the sever was updated to change an existing
    property to a symlink it would silently fail to create the symlink since
    the file is no longer there. This is no longer needed since now we clear
    the state tree every boot (and require a reboot after updating the
    unit), and we now have proper error messages so it won't be hard to find
    the source of the issue if we encounter it.

--- hal/property_manager.c
+++ hal/property_manager.c
@@ -185,77 +185,85 @@ static void change_group_and_dir_perms_for_all(void) {
 
 // Helper function to make properties
 static void make_prop(prop_t *prop) {
-    const int CMD_LENGTH = 2*MAX_PATH_LEN + 100;
-    char cmd[CMD_LENGTH];
+    // The absolute path of the property to be created
     char path[MAX_PATH_LEN];
 
-    // Convert prop's path to an absolute path and copy it to path
+    // Convert prop's path to an absolute path and store it in path
     get_abs_path(prop, path, MAX_PATH_LEN);
 
     switch (prop->type) {
 
-    case PROP_TYPE_FILE: {
+        case PROP_TYPE_FILE: {
 
-        mode_t prop_permsions;
+            mode_t prop_permsions;
 
-        switch(prop->permissions) {
-            case RO:
-                prop_permsions = S_IRUSR | S_IRGRP | S_IROTH;
+            switch(prop->permissions) {
+                case RO:
+                    prop_permsions = S_IRUSR | S_IRGRP | S_IROTH;
+                    break;
+
+                case WO:
+                    prop_permsions = S_IWUSR | S_IWGRP | S_IWOTH;
+                    break;
+
+                case RW:
+                    prop_permsions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
+                    break;
+
+                default:
+                    PRINT(ERROR, "Invalid permissions for property %s, defaulting to RW\n", path);
+                    prop_permsions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
+                    break;
+            }
+
+            // Create the file for the property
+            int touch_p_r = touch_p(path);
+
+            if(touch_p_r < 0) {
+                PRINT(ERROR, "Failed to create file for %s due to: %s\n", path, strerror(-touch_p_r));
                 break;
+            }
 
-            case WO:
-                prop_permsions = S_IWUSR | S_IWGRP | S_IWOTH;
+            // Set permissions
+            int chmod_r = chmod(path, prop_permsions);
+
+            if(chmod_r < 0) {
+                PRINT(ERROR, "Failed set file permissions for %s due to: %s\n", path, strerror(errno));
                 break;
+            }
 
-            case RW:
-                prop_permsions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
-                break;
-
-            default:
-                PRINT(ERROR, "Invalid permissions for property %s, defaulting to RW\n", path);
-                prop_permsions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
-                break;
-        }
-
-        // Create the file for the property
-        int touch_p_r = touch_p(path);
-
-        if(touch_p_r < 0) {
-            PRINT(ERROR, "Failed to create file for %s due to: %s\n", path, strerror(-touch_p_r));
             break;
         }
+        case PROP_TYPE_SYMLINK: {
 
-        int chmod_r = chmod(path, prop_permsions);
+            char dir[MAX_PATH_LEN];
 
-        if(chmod_r < 0) {
-            PRINT(ERROR, "Failed set file permissions for %s due to: %s\n", path, strerror(errno));
+            // Create directory where the symlink is to be created
+            int simple_dirname_r = simple_dirname(dir, path);
+
+            if(simple_dirname_r < 0) {
+                PRINT(ERROR, "Failed to get directory for %s due to: %s\n", path, strerror(-simple_dirname_r));
+                break;
+            }
+
+            // Create parent directory
+            int mkdir_p_r = mkdir_p(dir);
+
+            if(mkdir_p_r < 0) {
+                PRINT(ERROR, "Failed to create directory %s for %s due to: %s\n", dir, path, strerror(-mkdir_p_r));
+                break;
+            }
+
+            // Create symlink
+            int symlink_r = symlink(prop->symlink_target, prop->path);
+
+            if(symlink_r < 0) {
+                PRINT(ERROR, "Failed to create symlink due to: %s\n", strerror(errno));
+                break;
+            }
+
             break;
         }
-
-        break;
-    }
-    case PROP_TYPE_SYMLINK:
-
-        // TODO: @CF: The preferred way to build a directory tree relative to
-        // some path would be to use mkdirat(2), openat(2), etc. Here, we don't
-        // even check return values, which can be dangerous.
-
-        // TODO: @CF: use mkdir(2)
-        // mkdir -p /home/root/state/*
-        snprintf(cmd, CMD_LENGTH, "mkdir -p %s", get_abs_dir(prop, path, MAX_PATH_LEN));
-        system(cmd);
-        // PRINT( VERBOSE,"executing: %s\n", cmd);
-
-        snprintf(cmd, sizeof(cmd), "rm -Rf " STATE_DIR "/%s", prop->path);
-        system(cmd);
-
-        // TODO: replace with symlinkat(2)
-        snprintf(cmd, sizeof(cmd), "cd " STATE_DIR "; ln -sf " STATE_DIR "/%s %s",
-                 prop->symlink_target, prop->path);
-        system(cmd);
-        // PRINT( VERBOSE,"executing: %s\n", cmd);
-
-        break;
     }
 }
 
